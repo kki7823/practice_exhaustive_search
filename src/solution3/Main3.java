0515a7914d629511ec4cdb7fd17aa87eee18b017
gikim <kjd55399997@gmail.com>
2021-09-25 16:01:28 +0900

    210925

--- src/solution3/Main3.java
+++ src/solution3/Main3.java
@@ -1,4 +1,14 @@
 package solution3;
 
+import java.util.Arrays;
+
 public class Main3 {
+    public static void main(String[] args) {
+        int brown = 24;
+        int yellow = 24;
+
+        Solution3 sol = new Solution3();
+        int[] answer = sol.solution(brown, yellow);
+        System.out.println(Arrays.toString(answer));
+    }
 }
--- src/solution3/Solution3.java
+++ src/solution3/Solution3.java
@@ -1,4 +1,26 @@
 package solution3;
 
 public class Solution3 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+        int width = 0;
+        int height = 0;
+
+        for (int x = 3; x <= (brown + yellow) / 3; x++) {
+            if ((brown + yellow) % x != 0) continue;
+
+            int y = (brown + yellow) / x;
+            if ((x * 2) + (y * 2) - 4 == brown) {
+                width = Math.max(x,y);
+                height = Math.min(x,y);
+                break;
+            }
+        }
+
+
+        int[] result = {width, height};
+        answer = result;
+        return answer;
+    }
 }
